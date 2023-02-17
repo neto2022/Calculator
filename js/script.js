4ce00da7b23d5c9df48d7dcb12d4a3c1bde78e18
Neto <tabalneto14@gmail.com>
2023-02-17 16:08:02 -0400

    Code fixed using ESLint

--- js/script.js
+++ js/script.js
@@ -1,45 +1,47 @@
-var result = document.getElementById('textResult');
-var element = document.querySelectorAll('button');
-
-element.forEach(element => {
-    element.addEventListener('click', e => {
-        var resultE = e.target.dataset.value;
-        var splited;
-
-        if (resultE == '=') {
-            if (result.innerText.includes('x')) {
-                splited = result.innerText.split('x');
-                splited[2] = 'x'
-            } else if (result.innerText.includes('/')) {
-                splited = result.innerText.split('/');
-                splited[2] = '/'
-            } else if (result.innerText.includes('+')) {
-                splited = result.innerText.split('+');
-                splited[2] = '+'
-            } else if (result.innerText.includes('-')) {
-                splited = result.innerText.split('-');
-                splited[2] = '-'
-            }
-            operators(splited[0], splited[2], splited[1]);
-        }
-        if (resultE !== '=') {
-            result.innerText += resultE;
-        }
-        
-        if (resultE == 'C') {
-            return result.textContent = '';
-        }
-    })
-})
+const result = document.getElementById('textResult');
+const element = document.querySelectorAll('button');
 
 function operators(v1, op, v2) {
-    if (op == 'x') {
-        return result.textContent = v1 * v2;
-    } else if (op == '/') {
-        return result.textContent = (v1 / v2).toFixed(2);
-    } else if (op == '+') {
-        return result.textContent = v1 + v2;
-    } else if (op == '-') {
-        return result.textContent = v1 - v2;
-    }
+  if (op === 'x') {
+    result.textContent = v1 * v2;
+  }
+  if (op === '/') {
+    result.textContent = (v1 / v2).toFixed(2);
+  } if (op === '+') {
+    result.textContent = parseInt(v1, 10) + parseInt(v2, 10);
+  } if (op === '-') {
+    result.textContent = v1 - v2;
+  }
 }
+
+element.forEach((RandomElement) => {
+  RandomElement.addEventListener('click', (e) => {
+    const resultE = e.target.dataset.value;
+    let splited;
+
+    if (resultE === '=') {
+      if (result.innerText.includes('x')) {
+        splited = result.innerText.split('x');
+        splited[2] = 'x';
+      } else if (result.innerText.includes('/')) {
+        splited = result.innerText.split('/');
+        splited[2] = '/';
+      } else if (result.innerText.includes('+')) {
+        splited = result.innerText.split('+');
+        splited[2] = '+';
+      } else if (result.innerText.includes('-')) {
+        splited = result.innerText.split('-');
+        splited[2] = '-';
+      }
+
+      operators(splited[0], splited[2], splited[1]);
+    }
+    if (resultE !== '=') {
+      result.innerText += resultE;
+    }
+
+    if (resultE === 'C') {
+      result.textContent = '';
+    }
+  });
+});
